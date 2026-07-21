d97cd021e98d9ec47159ae5d32e3750b71056f91
Kalantar <kalantar@caspco.ir>
2026-07-21 11:09:46 +0330

    a30: first try

--- exercises/activities/src/bin/a30.rs
+++ exercises/activities/src/bin/a30.rs
@@ -4,7 +4,7 @@
 // * Create a Vehicle structure that is generic over traits Body and Color
 // * Create structures for vehicle bodies and vehicle colors and implement the
 //   Body and Color traits for these structures
-// * Implement a 'new' function for Vehicle that allows it to have any body
+// * Implement a 'new' function for Vehicle that allows it to have anybody
 //   and any color
 // * Create at least two different vehicles in the main function and print their
 //   info
@@ -18,4 +18,39 @@
 trait Body {}
 trait Color {}
 
-fn main() {}
+#[derive(Debug)]
+struct Vehicle<T: Body, U: Color> {
+    body: T,
+    color: U,
+}
+
+#[derive(Debug)]
+enum VehicleBody {
+    Truck,
+    Car,
+    Scooter,
+}
+
+#[derive(Debug)]
+enum VehicleColor {
+    Red,
+    White,
+    Black,
+}
+
+impl Body for VehicleBody {}
+impl Color for VehicleColor {}
+
+impl Vehicle<VehicleBody, VehicleColor> {
+    fn new(body: VehicleBody, color: VehicleColor) -> Self {
+        Self { body, color }
+    }
+}
+
+fn main() {
+    let benz = Vehicle::new(VehicleBody::Truck, VehicleColor::White);
+    println!("{:?}", benz);
+
+    let bmw = Vehicle::new(VehicleBody::Car, VehicleColor::Black);
+    println!("{:?}", bmw);
+}
