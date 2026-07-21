6cb36690ad05cbbaf8934611ac5e9a4c88948e3a
Kalantar <kalantar@caspco.ir>
2026-07-21 17:04:13 +0330

    a31: my impl

--- exercises/activities/src/bin/a31.rs
+++ exercises/activities/src/bin/a31.rs
@@ -19,4 +19,47 @@
 // * Use a function to calculate the total cost
 // * Process at least 3 different materials
 
-fn main() {}
+trait Material {
+    fn cost(&self) -> f64;
+}
+
+struct Carpet(f64);
+struct Tile(f64);
+struct Wood(f64);
+
+impl Material for Carpet {
+    fn cost(&self) -> f64 {
+        self.0 * 10.0
+    }
+}
+
+impl Material for Tile {
+    fn cost(&self) -> f64 {
+        self.0 * 15.0
+    }
+}
+
+impl Material for Wood {
+    fn cost(&self) -> f64 {
+        self.0 * 20.0
+    }
+}
+
+fn total(materials: Vec<Box<dyn Material>>) -> f64 {
+    let mut total = 0.0;
+    for m in materials {
+        total += m.cost();
+    }
+
+    total
+}
+
+fn main() {
+    let materials: Vec<Box<dyn Material>> = vec![
+        Box::new(Carpet(5.0)),
+        Box::new(Tile(12.0)),
+        Box::new(Wood(1.0)),
+    ];
+
+    println!("Total: {:?}", total(materials));
+}
