53f199126b5d8504de35ccf49be7b497dd2d0f74
Kalantar <kalantar@caspco.ir>
2026-07-22 17:20:01 +0330

    a32a: solve a32 according to instructor video

---
+++ exercises/activities/src/bin/a32a.rs
@@ -0,0 +1,41 @@
+// Topic: Lifetimes & Structures
+//
+// Requirements:
+// * Display just the names and titles of persons from the mock-data.csv file
+// * The names & titles must be stored in a struct separately from the mock
+//   data for potential later usage
+// * None of the mock data may be duplicated in memory
+//
+// Notes:
+// * The mock data has already been loaded with the include_str! macro, so all functionality
+//   must be implemented using references/borrows
+
+const MOCK_DATA: &'static str = include_str!("mock-data.csv");
+
+struct Names<'a> {
+    collection: Vec<&'a str>,
+}
+
+struct Titles<'a> {
+    collection: Vec<&'a str>,
+}
+
+fn main() {
+    let data: Vec<_> = MOCK_DATA.split('\n').skip(1).collect();
+    let names: Vec<_> = data
+        .iter()
+        .filter_map(|line| line.split(',').nth(1))
+        .collect();
+    let names = Names { collection: names };
+
+    let titles: Vec<_> = data
+        .iter()
+        .filter_map(|line| line.split(',').nth(4))
+        .collect();
+    let titles = Titles { collection: titles };
+    let data = names.collection.iter().zip(titles.collection.iter());
+
+    for (name, title) in data {
+        println!("Name: {}  Title: {}", name, title);
+    }
+}
