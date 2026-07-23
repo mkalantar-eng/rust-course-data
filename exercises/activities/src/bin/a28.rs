1b209e915174a4a5e583e8fc1c56af39fcdd01f0
Kalantar <mostafa.kalantar@gmail.com>
2026-07-23 22:27:58 +0330

    a28: my solution

--- exercises/activities/src/bin/a28.rs
+++ exercises/activities/src/bin/a28.rs
@@ -8,8 +8,9 @@
 // * Create a new type for each clothing item that wraps the Color enum
 //   * Each new type should implement a `new` function
 // * Create a function for each type of clothes (shoes, shirt, pants)
-//   that accepts the new type specific to that type of clothing
+//   that accepts the new type color specific to that type of clothing
 
+#[derive(Debug)]
 enum Color {
     Black,
     Blue,
@@ -23,4 +24,47 @@ enum Color {
     Yellow,
 }
 
-fn main() {}
+#[derive(Debug)]
+struct Shoe(Color);
+impl Shoe {
+    fn new(color: Color) -> Self {
+        Self(color)
+    }
+
+    fn set_color(&mut self, color: Color) {
+        self.0 = color;
+    }
+}
+#[derive(Debug)]
+struct Shirt(Color);
+impl Shirt {
+    fn new(color: Color) -> Self {
+        Self(color)
+    }
+
+    fn set_color(&mut self, color: Color) {
+        self.0 = color;
+    }
+}
+#[derive(Debug)]
+struct Pants(Color);
+impl Pants {
+    fn new(color: Color) -> Self {
+        Self(color)
+    }
+
+    fn set_color(&mut self, color: Color) {
+        self.0 = color;
+    }
+}
+fn main() {
+    let brown_shirt = Shirt::new(Color::Brown);
+    let red_shoe = Shoe::new(Color::Red);
+    let mut custom_pants = Pants::new(Color::Custom("Deep blue".to_string()));
+
+    println!("{:?}", brown_shirt);
+    println!("{:?}", red_shoe);
+    println!("{:?}", custom_pants);
+    custom_pants.set_color(Color::Blue);
+    println!("{:?}", custom_pants);
+}
