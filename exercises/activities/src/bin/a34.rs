ea00f5b73ec2a388f1ada5e06dd6dbad8005d706
Kalantar <mostafa.kalantar@gmail.com>
2026-07-24 11:24:49 +0330

    a34a: instructor version of a34

--- exercises/activities/src/bin/a34.rs
+++ exercises/activities/src/bin/a34.rs
@@ -16,4 +16,62 @@
 // Notes:
 // * Optionally use generics for each state
 
-fn main() {}
+#[derive(Debug)]
+struct Luggage<T> {
+    tracking_id: String,
+    state: T,
+}
+#[derive(Debug)]
+struct CheckIn;
+impl Luggage<CheckIn> {
+    fn new(tracking_id: String) -> Self {
+        Self {
+            tracking_id,
+            state: CheckIn,
+        }
+    }
+    fn load(self) -> Luggage<OnLoading> {
+        Luggage::<OnLoading> {
+            tracking_id: self.tracking_id,
+            state: OnLoading,
+        }
+    }
+}
+#[derive(Debug)]
+struct OnLoading;
+impl Luggage<OnLoading> {
+    fn offload(self) -> Luggage<Offloading> {
+        Luggage::<Offloading> {
+            tracking_id: self.tracking_id,
+            state: Offloading,
+        }
+    }
+}
+#[derive(Debug)]
+struct Offloading;
+impl Luggage<Offloading> {
+    fn await_pickup(self) -> Luggage<AwaitingPickup> {
+        Luggage::<AwaitingPickup> {
+            tracking_id: self.tracking_id,
+            state: AwaitingPickup,
+        }
+    }
+}
+#[derive(Debug)]
+struct AwaitingPickup;
+impl Luggage<AwaitingPickup> {
+    fn end_custody(self) -> Luggage<EndCustody> {
+        Luggage::<EndCustody> {
+            tracking_id: self.tracking_id,
+            state: EndCustody,
+        }
+    }
+}
+#[derive(Debug)]
+struct EndCustody;
+
+fn main() {
+    let luggage = Luggage::new("ftre44380k".to_string());
+    let c = luggage.load().offload().await_pickup().end_custody();
+    println!("{:?}", c);
+}
